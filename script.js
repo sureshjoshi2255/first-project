2c546b9781869045819e33af9ef58be1d2de6032
sureshjoshi2255 <sureshjoshi2072@gmail.com>
2026-07-07 00:40:42 +0900

    check

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const reservationForm = document.getElementById('reservation-form');
 const reservationMessage = document.getElementById('reservation-message');
 
+
 if (reservationForm) {
   reservationForm.addEventListener('submit', (event) => {
     event.preventDefault();
